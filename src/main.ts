3d3c800604fd6db289f63d887328f208f45877c0
Daniel Vitor <daniel.santos@brius.com.br>
2022-03-13 20:50:18 -0300

    feat: add helmet and setglobalprefix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'PUT', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    preflightContinue: false,
+  })
+  app.setGlobalPrefix('pokedex-api');
+  
   await app.listen(3000);
 }
+
 bootstrap();
