989b271763434b2f52944b720dfbc8f5748021d6
Daniel Vitor <daniel.santos@brius.com.br>
2022-03-27 20:55:18 -0300

    feat: change getPokemon

--- src/modules/pokemon/pokemon.controller.ts
+++ src/modules/pokemon/pokemon.controller.ts
@@ -4,13 +4,26 @@ import { PokemonService } from './pokemon.service';
 @Controller('pokemon')
 export class PokemonController {
     private offset = 0;
+    private pokemons = [];
 
     constructor(private readonly pokemonService: PokemonService) {}
 
     @Get()
-    async getPokemon() {
+    async getPokemon(loadMore = true) {
+        if (loadMore) {
+            this.offset += 25;
+        }
+
         const resp = await this.pokemonService.getPokemon(this.offset);
-        return resp;
+        resp.subscribe(resp => {
+            if (this.offset > 0) {
+                this.pokemons = [...this.pokemons, ...resp];
+            } else {
+                this.pokemons = resp;
+            }
+        });
+        console.log(this.pokemons);
+        return this.pokemons;
     }
 
     @Get('all')
